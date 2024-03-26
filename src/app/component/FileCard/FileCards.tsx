93e297c884e7e4de4015ff049ec9114c1969ab89
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-03-26 22:53:53 +0600

    maked full card of file

--- src/app/component/FileCard/FileCards.tsx
+++ src/app/component/FileCard/FileCards.tsx
@@ -1,39 +1,44 @@
 "use client";
-import { Grid, Paper, Typography,makeStyles } from "@mui/material";
+import { Grid, Paper, Typography, withStyles } from "@mui/material";
 import React from "react";
 import { SAMPLE_DATA } from "./SampleData";
 import { FileData } from "../../../../types/fileCard";
 import Image from "next/image";
-import Divider from '@mui/material/Divider';
-import WindowIcon from '@mui/icons-material/Window';
-import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import Divider from "@mui/material/Divider";
+import WindowIcon from "@mui/icons-material/Window";
+import CloudDownloadIcon from "@mui/icons-material/CloudDownload";
+import Rating from "@mui/material/Rating";
+import StarIcon from "@mui/icons-material/Star";
 
+// Function to calculate the fractional part of a number
+const fractionalPart = (num: number) => num - Math.floor(num);
+
+// Function to render the custom rating
+const CustomRating = ({ value }: { value: number }) => {
+  return (
+    <Rating
+      name="custom-rating"
+      value={value}
+      precision={0.1}
+      readOnly
+      icon={<StarIcon />}
+    />
+  );
+};
 export default function FileCards() {
-    const verticalLineStyle = {
-        height: '100%',
-        width: '2px', // Adjust the width as needed
-        backgroundColor: '#d9d9d9', // Change color as needed
-        margin: '0 10px', // Adjust margin as needed
-      };
-        // Check if the device size is large (lg)
-  const isLargeDevice = window.innerWidth >= 1280; // Adjust the breakpoint as needed
 
-  // Apply height only for large devices
-  if (isLargeDevice) {
-    verticalLineStyle.height = '60px';
-  }
   return (
     <>
-      {SAMPLE_DATA.map((data: FileData, index: number) => {
-        return (
-          <Grid alignItems={'center'} key={index} gap={3} container>
+          <Grid alignItems={"center"}  gap={3} container>
             {" "}
-            <Grid xs={0} sm={0} lg={1}></Grid>
-            {/* 1st data list  */}
-            <Grid xs={12} lg={5}>
+            <Grid xs={0} sm={0} xl={0.5}></Grid>
+            <Grid container xs={11} gap={3}>
+            {SAMPLE_DATA.map((data: FileData, index: number) => {
+        return (
+            <Grid key={index} xs={12} xl={5.5} >
               <Paper sx={{ p: 1 }} elevation={3}>
                 <Grid container>
-                  <Grid  xs={12} lg={1.2}>
+                  <Grid  xs={12} sm={1}>
                     <Image
                       alt={data.title}
                       src={data.image}
@@ -42,40 +47,75 @@ export default function FileCards() {
                     />
                   </Grid>
 
-                  <Grid xs={12} lg={7}>
-                     <Typography sx={{fontWeight: 600}}>{data.title}</Typography>
-                     <Typography sx={{fontWeight: 300,fontSize: 12}}>{data.description}</Typography>
-                     <Typography sx={{fontWeight: 300,fontSize: 12,color:"#239436"}}>{data.category.subCategory.name}</Typography>
+                  <Grid sx={{borderRight:"2px solid lightGray"}} xs={12} sm={5}>
+                    <Typography sx={{ fontWeight: 600 }}>
+                      {data.title}
+                    </Typography>
+                    <Typography sx={{ fontWeight: 300, fontSize: 12 }}>
+                      {data.description}
+                    </Typography>
+                    <Typography
+                      sx={{ fontWeight: 300, fontSize: 12, color: "#239436" }}
+                    >
+                      {data.category.subCategory.name}
+                    </Typography>
                   </Grid>
-                  <Divider  orientation="horizontal" style={verticalLineStyle} />
-                  <Grid xs={12} lg={1}>
-                     <Typography sx={{fontWeight: 600,display:"flex"}}><span><WindowIcon sx={{color:"#0080ff"}} /></span><span>{data.category.primary.name}</span></Typography> 
-                     <Typography sx={{display:"flex",gap:1,alignItems:"center",fontSize:13}}><CloudDownloadIcon sx={{color:"#bdbdbd"}} /> <span>{data.totalDownload}</span></Typography>
+                  <Grid xs={0.2}></Grid>
+                  
+                  <Grid sx={{borderRight:"2px solid lightGray"}} xs={12} sm={2}>
+                    <Typography sx={{ fontWeight: 600, display: "flex" }}>
+                      <span>
+                        <WindowIcon sx={{ color: "#0080ff" }} />
+                      </span>
+                      <span>{data.category.primary.name}</span>
+                    </Typography>
+                    <Typography
+                      sx={{
+                        display: "flex",
+                        gap: 1,
+                        alignItems: "center",
+                        fontSize: 13,
+                      }}
+                    >
+                      <CloudDownloadIcon sx={{ color: "#bdbdbd" }} />{" "}
+                      <span>{data.totalDownload}</span>
+                    </Typography>
                   </Grid>
-
-
+                  <Grid xs={0.2}></Grid>
+                  
+                  <Grid sx={{borderRight:"2px solid lightGray"}} xs={6} sm={2.3}>
+                    <Typography sx={{ fontWeight: 600, display: "flex" }}>
+                      Rating
+                    </Typography>
+                    <Typography
+                      sx={{
+                        display: "flex",
+                        gap: 1,
+                        alignItems: "center",
+                        fontSize: 13,
+                      }}
+                    >
+                      {" "}
+                      <span>
+                        <CustomRating value={data.rating} />
+                      </span>
+                    </Typography>
+                  </Grid>
+                  <Grid xs={0.2}></Grid>
+                  <Grid sx={{alignItems:"center", display:"flex"}} xs={6} sm={1}>
+                    <Typography sx={{ fontWeight: 600, display: "flex",alignItems:"center" }}>
+                      {data.mbSize}  MB
+                    </Typography>
+                     
+                  </Grid>
                 </Grid>
               </Paper>
             </Grid>
-            {/* 2nd data list  */}
-            <Grid xs={12} lg={5}>
-              <Paper sx={{ p: 1 }} elevation={3}>
-                <Grid container>
-                  <Grid xs={2}>
-                    <Image
-                      alt={data.title}
-                      src={data.image}
-                      width={60}
-                      height={60}
-                    />
-                  </Grid>
-                </Grid>
-              </Paper>
+            );
+          })}
             </Grid>
-            <Grid xs={0} sm={0} lg={1}></Grid>
-          </Grid>
-        );
-      })}
+          <Grid xs={0} sm={0} lg={0.5}></Grid>
+        </Grid>
     </>
   );
 }
--- src/app/component/FileCard/SampleData.ts
+++ src/app/component/FileCard/SampleData.ts
@@ -1,78 +1,78 @@
 import { FileData } from "../../../../types/fileCard";
 
-export const SAMPLE_DATA:FileData[] = [
-    {
-        "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
-        "title": "Example File 1",
-        "description": "This is a sample description for Example File 1.",
-        "category": {
-          "primary": {
-            "name": "Windows",
-            "type": "windows"
-          },
-          "subCategory": {
-            "name": "Utilities",
-            "type": "System Tools"
-          }
-        },
-        "totalDownload": 1000,
-        "rating": 4.5,
-        "mbSize": 10.2
+export const SAMPLE_DATA: FileData[] = [
+  {
+    "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
+    "title": "Example File 1",
+    "description": "This is a sample description for Example File 1.",
+    "category": {
+      "primary": {
+        "name": "Windows",
+        "type": "windows"
       },
-    {
-        "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
-        "title": "Example File 1",
-        "description": "This is a sample description for Example File 1.",
-        "category": {
-          "primary": {
-            "name": "Windows",
-            "type": "windows"
-          },
-          "subCategory": {
-            "name": "Utilities",
-            "type": "System Tools"
-          }
-        },
-        "totalDownload": 1000,
-        "rating": 4.5,
-        "mbSize": 10.2
-      },
-    {
-        "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
-        "title": "Example File 1",
-        "description": "This is a sample description for Example File 1.",
-        "category": {
-          "primary": {
-            "name": "Windows",
-            "type": "windows"
-          },
-          "subCategory": {
-            "name": "Utilities",
-            "type": "System Tools"
-          }
-        },
-        "totalDownload": 1000,
-        "rating": 4.5,
-        "mbSize": 10.2
-      },
-      {
-        "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
-        "title": "Example File 2",
-        "description": "This is a sample description for Example File 2.",
-        "category": {
-          "primary": {
-            "name": "Windows",
-            "type": "windows"
-          },
-          "subCategory": {
-            "name": "Editing",
-            "type": "Video Editors"
-          }
-        },
-        "totalDownload": 500,
-        "rating": 3.8,
-        "mbSize": 25.7
+      "subCategory": {
+        "name": "Utilities",
+        "type": "System Tools"
       }
-      
-      
+    },
+    "totalDownload": 1000,
+    "rating": 4.5,
+    "mbSize": 10.2
+  },
+  {
+    "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
+    "title": "Example File 2",
+    "description": "This is a sample description for Example File 1.",
+    "category": {
+      "primary": {
+        "name": "Windows",
+        "type": "windows"
+      },
+      "subCategory": {
+        "name": "Utilities",
+        "type": "System Tools"
+      }
+    },
+    "totalDownload": 1000,
+    "rating": 4.5,
+    "mbSize": 10.2
+  },
+  {
+    "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
+    "title": "Example File 3",
+    "description": "This is a sample description for Example File 1.",
+    "category": {
+      "primary": {
+        "name": "Windows",
+        "type": "windows"
+      },
+      "subCategory": {
+        "name": "Utilities",
+        "type": "System Tools"
+      }
+    },
+    "totalDownload": 1000,
+    "rating": 4.5,
+    "mbSize": 10.2
+  },
+  {
+    "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
+    "title": "Example File 4",
+    "description": "This is a sample description for Example File 2.",
+    "category": {
+      "primary": {
+        "name": "Windows",
+        "type": "windows"
+      },
+      "subCategory": {
+        "name": "Editing",
+        "type": "Video Editors"
+      }
+    },
+    "totalDownload": 500,
+    "rating": 1.9,
+    "mbSize": 25.7
+  }
+
+
 ]
