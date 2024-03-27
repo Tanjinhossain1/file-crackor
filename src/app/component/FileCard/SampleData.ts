5ed3ec22052d8ff50ca89f364be9f43a6e51b350
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-03-27 20:12:13 +0600

    add search fields

--- src/app/component/FileCard/SampleData.ts
+++ src/app/component/FileCard/SampleData.ts
@@ -152,7 +152,7 @@ export const SAMPLE_DATA_OF_WINDOW: FileData[] = [
 
 export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
   {
-    "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Ftechsmith-camtasia-1002%2Ftechsmith-camtasia-logo.png&w=64&q=75",
     "title": "Example File 1",
     "description": "This is a sample description for Example File 1.",
     "category": {
@@ -170,7 +170,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 10.2
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Faffinity-designer%2F64660dd5f0ddb-affinity-designer-Icon.webp&w=64&q=75",
     "title": "Example File 2",
     "description": "This is a sample description for Example File 1.",
     "category": {
@@ -188,7 +188,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 10.2
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-designer%2FSerif-Affinity-Designer-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Faffinity-photo%2F64660f63b6512-affinity-photo-Icon.webp&w=64&q=75",
     "title": "Example File 3",
     "description": "This is a sample description for Example File 1.",
     "category": {
@@ -206,7 +206,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 10.2
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-publisher%2FAffinity_Publisher_Logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fluminar-neo-1003%2FLuminar-neo-for-macos-logo.png&w=64&q=75",
     "title": "Example File 4",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -224,7 +224,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Feaseus-partition-master%2FEaseUS-Partition-Master.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fwondershare-pdfelement-pro-ocr-1001%2Fwondershare-pdfelement-pro-ocr-for-mac-logo.png&w=64&q=75",
     "title": "Example File 5",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -242,7 +242,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fautodesk-inventor%2FAutodesk-Inventor-Professional.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fcharliemonroe-downie-1001%2Fdownie-for-mac-logo.png&w=64&q=75",
     "title": "Example File 6",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -260,7 +260,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Foffice-r-tool%2Fofficertool-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fwaves-complete-14-v170123%2Fwaves14.png&w=64&q=75",
     "title": "Example File 7",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -278,7 +278,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fcapcut%2Fcapcut-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F07%2Fopenmedia-4k-video-downloader-plus-pro%2F64b52489e3b76-openmedia-4k-video-downloader-plus-pro-Icon.webp&w=64&q=75",
     "title": "Example File 8",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -301,7 +301,7 @@ export const SAMPLE_DATA_OF_MACOS: FileData[] =  [
 
 export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
   {
-    "image": "https://media.imgcdn.org/repo/2023/03/malwarebytes/Malwarebytes_Logo.png",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fphoto-translator%2F64477dceba849-photo-translator-Icon.png&w=64&q=75",
     "title": "Example File 1",
     "description": "This is a sample description for Example File 1.",
     "category": {
@@ -319,7 +319,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 10.2
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-photo%2FSerif-Affinity-Photo-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fpdf-extra-scan-edit-and-sign%2F65571280d9523-pdf-extra-scan-edit-and-sign-Icon.webp&w=64&q=75",
     "title": "Example File 2",
     "description": "This is a sample description for Example File 1.",
     "category": {
@@ -337,7 +337,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 10.2
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-designer%2FSerif-Affinity-Designer-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F04%2Fyoutube-revanced-extended%2F6447a17c5cc4f-youtube-revanced-extended-Icon.png&w=64&q=75",
     "title": "Example File 3",
     "description": "This is a sample description for Example File 1.",
     "category": {
@@ -355,7 +355,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 10.2
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fserif-affinity-publisher%2FAffinity_Publisher_Logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fwps-office-pdf-word-excel-ppt%2Fwps-office-app-logo.png&w=64&q=75",
     "title": "Example File 4",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -373,7 +373,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Feaseus-partition-master%2FEaseUS-Partition-Master.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fpicsart-ai-photo-editor-video%2F65d34faac8f9a-picsart-ai-photo-editor-video-Icon.webp&w=64&q=75",
     "title": "Example File 5",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -391,7 +391,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fautodesk-inventor%2FAutodesk-Inventor-Professional.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fsolid-explorer-file-manager%2Fsolid-explorer-file-manager-Logo.png&w=64&q=75",
     "title": "Example File 6",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -409,7 +409,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Foffice-r-tool%2Fofficertool-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fspeed-reading-brain-training%2FSpeed-reading-brain-training-logo.png&w=64&q=75",
     "title": "Example File 7",
     "description": "This is a sample description for Example File 2.",
     "category": {
@@ -427,7 +427,7 @@ export const SAMPLE_DATA_OF_ANDROID: FileData[] =  [
     "mbSize": 25.7
   },
   {
-    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fcapcut%2Fcapcut-logo.png&w=64&q=75",
+    "image": "https://filecr.com/_next/image/?url=https%3A%2F%2Fmedia.imgcdn.org%2Frepo%2F2023%2F03%2Fccleaner-phone-cleaner%2Fccleaner-phone-cleaner-icon.png&w=64&q=75",
     "title": "Example File 8",
     "description": "This is a sample description for Example File 2.",
     "category": {
--- src/app/component/Navbar/index.tsx
+++ src/app/component/Navbar/index.tsx
@@ -15,43 +15,35 @@ import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 import AlignHorizontalLeftIcon from "@mui/icons-material/AlignHorizontalLeft";
 import { Grid } from "@mui/material";
+import SearchBar from "../SearchBar";
 
-const pages = [{
-    name:"Windows",
-    link:"/windows"
-},
-{
-    name:"MacOs",
-    link:"/macos"
-},
-{
-    name:"Android",
-    link:"/android"
-}
-] 
+const pages = [
+  {
+    name: "Windows",
+    link: "/windows",
+  },
+  {
+    name: "MacOs",
+    link: "/macos",
+  },
+  {
+    name: "Android",
+    link: "/android",
+  },
+];
 const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function Navbar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
@@ -60,60 +52,78 @@ function Navbar() {
         <Toolbar disableGutters>
           <AlignHorizontalLeftIcon
             sx={{ display: { xs: "none", md: "flex" }, mr: 15 }}
-          /> 
-              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" },ml:5,  ":hover":{bgcolor:"red"} }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: "block", md: "none" },
-                  }}
-                >
-                    
-                  {pages.map((page,index) => (
-                    <MenuItem key={index} onClick={handleCloseNavMenu}>
-                      <Typography textAlign="center">{page.name}</Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Box>
-             
+          />
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "none" },
+              ml: 5,
+              ":hover": { bgcolor: "red" },
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page, index) => (
+                <MenuItem key={index} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
           {/* mobile  */}
           <AlignHorizontalLeftIcon
             sx={{ display: { xs: "flex", md: "none" }, mr: 1 }}
           />
 
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }}}>
-            {pages.map((page,index) => ( 
-                <Button
+          <Box sx={{ flexGrow: 2, display: { xs: "none", md: "flex" },gap:4 }}>
+            {pages.map((page, index) => (
+              <Button
                 key={index}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block",":hover":{bgcolor:"white",color:"black"} }}
+                sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  ":hover": { bgcolor: "white", color: "black" },
+                }}
               >
                 {page.name}
               </Button>
             ))}
+            <Grid sx={{ mt: 1 }} container>
+              <Grid xs={3.7}></Grid>
+              <Grid xs={4}>
+                <SearchBar />
+              </Grid>
+              <Grid xs={4}></Grid>
+            </Grid>
           </Box>
         </Toolbar>
       </Container>
